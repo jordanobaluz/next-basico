b8f161cab295fe966b570da49fa738331cba6c7b
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-01 21:31:35 -0300

    Componente de navegação de forma programatica demonstrando a navegação entre paginas

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,10 +1,33 @@
 import Link from 'next/link'
+import router, { useRouter } from 'next/router'
 
 export default function rotas() {
+    const estilo = {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start"
+    }
+    //const router = useRouter()
+
+    function navegacaoSimples(url) {
+        router.push(url)
+    }
+
+    function navegacaoComParams() {
+        router.push({
+            pathname: "/rotas/params",
+            query: {
+                id: 2,
+                nome: 'Nicoly'
+            }
+        })
+    }
+
     return (
         <div>
             <h1>Rotas Index</h1>
             <p>O arquivo index é acessado por padrão, quando colocado somente o nome da pasta na URL</p>
+            <h3>Navegação utilizando Link</h3>
             <ul>
                 <Link href="/rotas/params?id=2&nome=Nicoly">
                     <li>Params</li>
@@ -16,6 +39,13 @@ export default function rotas() {
                     <li>Jordano</li>
                 </Link>
             </ul>
+            <div style={estilo}>
+                <h3>Navegação utilizando Router</h3>
+                <p>Demonstração de navegação de forma programatica</p>
+                <button onClick={() => navegacaoComParams()}>Params</button>
+                <button onClick={() => router.push("/rotas/123/buscar")}>Buscar</button>
+                <button onClick={() => navegacaoSimples("/rotas/123/Jordano")}>Jordano</button>
+            </div>
         </div >
     )
 }
